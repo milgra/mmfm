5039224482fae44153833c36d4b6748ed2eed73c
Milan Toth <milgra@milgra.com>
2022-11-15 15:59:22 +0000

    working tests

--- tst/test_run.sh
+++ tst/test_run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-exit 0
+cd ..
 
-./tst/test_rep.sh tst/ui_file
-./tst/test_rep.sh tst/ui_keyboard
-./tst/test_rep.sh tst/ui_mouse
+sh tst/test_rep.sh tst/ui_file
+sh tst/test_rep.sh tst/ui_keyboard
+sh tst/test_rep.sh tst/ui_mouse
+
+exit 0

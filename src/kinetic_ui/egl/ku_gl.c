0e832a40b776b2bab92c3feab2703a2c39dc0e41
Milan Toth <milgra@milgra.com>
2023-02-21 14:09:41 +0100

    determinism fix

--- src/kinetic_ui/egl/ku_gl.c
+++ src/kinetic_ui/egl/ku_gl.c
@@ -136,8 +136,9 @@ gltex_t ku_gl_create_texture(int index, uint32_t w, uint32_t h)
 
     glActiveTexture(GL_TEXTURE0 + tex.index);
     glBindTexture(GL_TEXTURE_2D, tex.tx);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    /* !!! GL_LINEAR breaks session record/replay determinism, interpolated values can somehow differ */
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
